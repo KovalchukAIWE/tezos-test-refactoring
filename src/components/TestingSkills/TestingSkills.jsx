f0edb077f69c2e600439562804c4b6ef8a1f4c06
Andrii Kovalchuk <kovalchukaiwe@gmail.com>
2021-11-05 08:58:43 +0200

    fix(components): fixed styles

--- src/components/TestingSkills/TestingSkills.jsx
+++ src/components/TestingSkills/TestingSkills.jsx
@@ -2,39 +2,39 @@ import React from "react";
 import styles from "./TestingSkills.module.scss";
 
 const TestingSkills = () => (
-  <div id='skills' className={styles.wrapper}>
-    <p className={styles.block_header}>Testing skils</p>
-    <div className={styles.wrapper_block}>
-      <div className={styles.left_block}>
-        <div className={styles.icon_text}>
-          <div className={styles.icon_check} />
-          <div className={styles.block_text}>
+  <div id='skills' className={styles.container}>
+    <p className={styles.header}>Testing skills</p>
+    <div className={styles.wrapper}>
+      <div className={styles.left}>
+        <div className={styles.check}>
+          <div className={styles.check__icon} />
+          <div className={styles.check__text}>
             Applying HTML, CSS/SASS. Markup implementation
           </div>
         </div>
-        <div className={styles.icon_text}>
-          <div className={styles.icon_check} />
-          <div className={styles.block_text}>
+        <div className={styles.check}>
+          <div className={styles.check__icon} />
+          <div className={styles.check__text}>
             Applying JS. Transforming and organising data. Interaction
           </div>
         </div>
       </div>
-      <div className={styles.statistic_block}>
-        <div className={styles.statistic_markup}>HTML, CSS/SASS</div>
-        <div className={styles.statistic_rest}>TTP/REST</div>
-        <div className={styles.statistic_js}>JS</div>
-        <div className={styles.statistic_framework}>VUE/REACT</div>
+      <div className={styles.statistic}>
+        <div className={styles.statistic__markup}>HTML, CSS/SASS</div>
+        <div className={styles.statistic__rest}>TTP/REST</div>
+        <div className={styles.statistic__js}>JS</div>
+        <div className={styles.statistic__framework}>VUE/REACT</div>
       </div>
-      <div className={styles.right_block}>
-        <div className={styles.icon_text}>
-          <div className={styles.icon_check} />
-          <div className={styles.block_text}>
+      <div className={styles.right}>
+        <div className={styles.check}>
+          <div className={styles.check__icon} />
+          <div className={styles.check__text}>
             Working with asynchronous code, HTTP and REST architecture
           </div>
         </div>
-        <div className={styles.icon_text}>
-          <div className={styles.icon_check} />
-          <div className={styles.block_text}>
+        <div className={styles.check}>
+          <div className={styles.check__icon} />
+          <div className={styles.check__text}>
             Using one of the modern front-end frameworks either Vue or React.
             Vue is preferable but React is also viable
           </div>
